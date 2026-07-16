ea6cf7571a9c33eac1530171d04f6455eb2ecbfa
tarekRoshdi2 <tarekroshdi@gmail.com>
2026-07-16 05:40:23 +0300

    0.0.0.4

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 // Entry point for Hostinger Node.js
 // Hostinger uses cPanel Passenger which looks for app.js or server.js by default.
+process.env.NODE_ENV = 'production';
 require('./dist/server.cjs');
